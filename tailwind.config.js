05d78096bf89b3d580e07e562ff56551e560108d
dev-acc-practice <development-practice@proton.me>
2022-11-02 14:31:50 +0200

    styled second row- added image and news block

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,16 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
-    extend: {
-      gridColumnStart: {
-        '1' : '1'
-      },
-      gridColumnEnd: {
-        '3' : '3'
-
-      },
-
+    colors : {
+      softorange: 'hsl(35, 77%, 62%)',
+      softred: 'hsl(5, 85%, 63%)',
+      offwhite: 'hsl(36, 100%, 99%)',
+      grayishblue: 'hsl(233, 8%, 79%)',
+      darkgrayishblue: 'hsl(236, 13%, 42%)',
+      verydarkblue: 'hsl(240, 100%, 5%)'
     },
+
+    extend: {},
   },
   plugins: [],
 }
