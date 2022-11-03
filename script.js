c8913b05b48521334852337b261e2c661f4a7f75
dev-acc-practice <development-practice@proton.me>
2022-11-03 07:35:41 +0200

    disabled and enabled body overflow when menu is toggled

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ open_menu.addEventListener('click', () =>{
     mobile_menu.style.display = 'block';
     close_menu.style.display = 'block';
     mobile_menu.classList.add('open');
+    document.body.style.overflow='hidden';
 });
 
 close_menu.addEventListener('click', () =>{
     mobile_menu.classList.remove('open');
     mobile_menu.style.display ='none';
     close_menu.style.display='none';
+    document.body.style.overflow='visible';
 });
